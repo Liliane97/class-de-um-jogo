e7ee7cddec53392e16aeb261700f0ecbfac9f4bc
liliane97 <lilianebasilio1997@gmail.com>
2023-10-08 21:12:51 -0300

    Criando uma classe de um jogo

---
+++ index.js
@@ -0,0 +1,29 @@
+class Heroi {
+
+    constructor(nome, idade, tipo) {
+        this.nome = nome,
+            this.idade = idade,
+            this.tipo = tipo
+    }
+
+    atacar() {
+
+        let ataque
+
+        if (this.tipo === "mago") {
+            ataque = "magia"
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada"
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais"
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken"
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+
+    }
+}
+
+const heroi = new Heroi("Ana", 18, "ninja")
+heroi.atacar()
